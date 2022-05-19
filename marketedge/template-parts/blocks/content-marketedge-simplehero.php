d09539ba4d12a098ff72b93c04797c05c6b3b5d2
Adam Bowen <adamcbowen81@hotmail.com>
2022-05-19 09:15:35 -0700

    Simple hero adjustment

--- marketedge/template-parts/blocks/content-marketedge-simplehero.php
+++ marketedge/template-parts/blocks/content-marketedge-simplehero.php
@@ -16,20 +16,22 @@ $heading = get_field('heading');
 $content = get_field('content');
 $cta = get_field('cta');
 ?>
-<section id="<?php echo $id; ?>" class="<?php echo $classes;?>" style="background-color: <?php echo $background_color;?>;background-image:url(<?php echo $background_image['url'];?>);background-size:cover;background-repeat:no-repeat;background-position: center;">  
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <?php if($heading): ?>
-                    <div class="hero-shape"></div>
-                    <h1><?php echo $heading;?></h1>
-                <?php endif; ?>
-                <?php if($content): ?>
-                    <p><?php echo $content;?></p>
-                <?php endif; ?>
-                <?php if($cta): ?>
-                    <a class="btn btn-primary" href="<?php echo $cta['url'];?>" target="<?php echo $cta['target'];?>"><?php echo $cta['title'];?></a>
-                <?php endif; ?>
+<section id="<?php echo $id; ?>" class="<?php echo $classes;?>">  
+    <div class="simple-hero-bg" style="background-color: <?php echo $background_color;?>;background-image:url(<?php echo $background_image['url'];?>);background-size:cover;background-repeat:no-repeat;background-position: center;">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <?php if($heading): ?>
+                        <div class="hero-shape"></div>
+                        <h1><?php echo $heading;?></h1>
+                    <?php endif; ?>
+                    <?php if($content): ?>
+                        <p><?php echo $content;?></p>
+                    <?php endif; ?>
+                    <?php if($cta): ?>
+                        <a class="btn btn-primary" href="<?php echo $cta['url'];?>" target="<?php echo $cta['target'];?>"><?php echo $cta['title'];?></a>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
     </div>
